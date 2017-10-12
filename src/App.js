ec77cabba6259e15f54dd523a8e810f485550c55
Mike Sizer <mikesizer@users.noreply.github.com>
2017-10-12 19:42:14 +0100

    Fixed indentation

--- src/App.js
+++ src/App.js
@@ -7,42 +7,42 @@ import styled, { keyframes } from 'styled-components';
 
 
 const Container = styled.div`
-	display: flex;
-	justify-content: center;
-	margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 `;
 
 const Flash = keyframes`
-	0% {
-		opacity: 0;
-	}
-	50% {
-		opacity: 0;
-	}
-	51% {
-		opacity: 1;
-	}
-	100% {
-		opacity: 1;
-	}
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0;
+  }
+  51% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
 `;
 
 const Indicator = styled.img`
-	opacity: 0;
+  opacity: 0;
   animation: ${props => (
-		props.blinking ? `${Flash} 0.8s infinite` : 'none'
-	)};
-	position: absolute;
-	left: 100px;
+    props.blinking ? `${Flash} 0.8s infinite` : 'none'
+  )};
+  position: absolute;
+  left: 100px;
 `;
 
 const Road = styled.img`
-	position: absolute;
-	margin-top: 256px;
+  position: absolute;
+  margin-top: 256px;
 `;
 
 const Speed = styled.p`
-	position: absolute;
+  position: absolute;
   margin-top: 64px;
   font-family: Helvetica;
   font-weight: bold;
@@ -51,13 +51,13 @@ const Speed = styled.p`
 `;
 
 const Controls = styled.div`
-	position: absolute;
-	bottom: 20px;
+  position: absolute;
+  bottom: 20px;
 `;
 
 const Button = styled.button`
-	cursor: pointer;
-	font-size: 15px;
+  cursor: pointer;
+  font-size: 15px;
 `;
 
 
@@ -67,51 +67,51 @@ class App extends Component {
 
     this.state = {
       blinking: false,
-			speed: 56,
+      speed: 56,
     }
   }
-	toggleIndicater = () => {
-		this.setState((prevState) => (
-			{
-				blinking: !prevState.blinking,
-				speed: 58,
-			}
-		));
-	}
+  toggleIndicater = () => {
+    this.setState((prevState) => (
+      {
+        blinking: !prevState.blinking,
+	speed: 58,
+      }
+    ));
+  }
   render() {
 
-		const {
-			state: {
-				blinking,
-				speed,
-			},
-			toggleIndicater,
-		} = this;
+    const {
+      state: {
+        blinking,
+	speed,
+      },
+      toggleIndicater,
+    } = this;
 
     return (
       <Container>
 
-				<Indicator
-					blinking={blinking}
-					src={indicatorIcon}
-					width="40"
-				/>
+        <Indicator
+          blinking={blinking}
+	  src={indicatorIcon}
+	  width="40"
+	/>
 
-				<Speed>{speed}</Speed>
+	<Speed>{speed}</Speed>
 
-				<img
-					src={infoIcon}
-					height="400"
-				/>
+	<img
+	  src={infoIcon}
+	  height="400"
+	/>
 
-				<Road
-					src={blinking ? changingLaneIcon : roadIcon}
-					height="100"
-				/>
+	<Road
+          src={blinking ? changingLaneIcon : roadIcon}
+	  height="100"
+	/>
 
-				<Controls>
-					<Button onClick={toggleIndicater}>Indicate left</Button>
-				</Controls>
+	<Controls>
+	  <Button onClick={toggleIndicater}>Indicate left</Button>
+	</Controls>
 
       </Container>
     );
